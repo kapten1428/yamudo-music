8025eb7891c59dd552cad9abfa5739f8961da414
kapten1428 <kapten1428@gmail.com>
2017-09-10 19:36:41 +0700

    fix not found image

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
                         options:{
                             name: '[name].[ext]',
                             outputPath: 'images/',
-                            publicPath: '/'
+                            publicPath: ''
                         }
                     }
 
