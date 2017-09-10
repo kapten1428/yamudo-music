6ba376e9ad780c73a1be17167d59838976f1a867
kapten1428 <kapten1428@gmail.com>
2017-09-11 00:55:54 +0700

    add docs/ folder

---
+++ docs/bundle.js
@@ -0,0 +1,158 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(1);
+
+var _mobileMenu = __webpack_require__(2);
+
+new _mobileMenu.mobileMenu();
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var mobileMenu = exports.mobileMenu = function () {
+    function mobileMenu() {
+        var _this = this;
+
+        _classCallCheck(this, mobileMenu);
+
+        this.openClick = document.getElementsByClassName('header__hero-menu--mobile--is-open');
+        this.closeClick = document.getElementsByClassName('header__hero-menu--mobile--is-close');
+        this.openMenu = document.getElementsByClassName('nav-menu-mobile');
+        this.getStart = document.getElementsByClassName('header__icon-start');
+
+        this.openClick[0].addEventListener('click', function () {
+            return _this.onClickMenu();
+        });
+        this.closeClick[0].addEventListener('click', function () {
+            return _this.onCloseMenu();
+        });
+        this.getStart[0].addEventListener('click', function () {
+            return _this.moveMenu();
+        });
+    }
+
+    _createClass(mobileMenu, [{
+        key: 'onClickMenu',
+        value: function onClickMenu() {
+
+            this.openClick[0].style.fontSize = '0';
+            this.openClick[0].style.transition = 'font-size .4s';
+
+            this.closeClick[0].style.fontSize = '1em';
+            this.closeClick[0].style.display = "block";
+
+            this.openMenu[0].style.transition = 'opacity 1s';
+            this.openMenu[0].style.opacity = 1;
+        }
+    }, {
+        key: 'onCloseMenu',
+        value: function onCloseMenu() {
+            this.openClick[0].style.display = "block";
+            this.openClick[0].style.fontSize = '1em';
+
+            this.closeClick[0].style.fontSize = 0;
+            this.closeClick[0].style.transition = 'font-size .5s';
+            this.openMenu[0].style.opacity = 0;
+        }
+    }, {
+        key: 'moveMenu',
+        value: function moveMenu() {
+
+            this.getStart[0].setAttribute('class', 'header__icon-start header__icon-start--hidden');
+            this.getStart[0].innerHTML = 'Go';
+        }
+    }]);
+
+    return mobileMenu;
+}();
+
+/***/ })
+/******/ ]);
