7c4f882627e946eee9fc686fac95806fcd1e5dfa
kapten1428 <kapten1428@gmail.com>
2017-09-12 21:55:57 +0700

    update menu

--- app/scripts/mobileMenu.js
+++ app/scripts/mobileMenu.js
@@ -1,48 +1,17 @@
 export class mobileMenu {
 
-
     constructor(){
-       this.openClick = document.getElementsByClassName('header__hero-menu--mobile--is-open');
-       this.closeClick = document.getElementsByClassName('header__hero-menu--mobile--is-close');
-       this.openMenu = document.getElementsByClassName('nav-menu-mobile');
-       this.getStart = document.getElementsByClassName('header__icon-start');
-
-       this.openClick[0].addEventListener('click',() => this.onClickMenu());
-       this.closeClick[0].addEventListener('click',() => this.onCloseMenu());
-       this.getStart[0].addEventListener('click',() => this.moveMenu());
+        this.getIcon = document.querySelector('.header__icon-menu');
+        this.getMenuMobile = document.querySelector('.header');
+        this.getMenuMobileList= document.querySelector('.header__nav-bar');
+        this.getIcon.addEventListener('click',() => this.onClickMenu())
     }
-
     onClickMenu(){
-
-        this.openClick[0].style.fontSize = '0';
-        this.openClick[0].style.transition = 'font-size .4s';
-
-        this.closeClick[0].style.fontSize = '1em';
-        this.closeClick[0].style.display = "block";
-
-        this.openMenu[0].style.transition = 'opacity 1s';
-        this.openMenu[0].style.opacity = 1;
+        this.getIcon.classList.toggle('header__icon-menu--is-open')
+        this.getMenuMobile.classList.toggle('header__menu-open');
+        this.getMenuMobileList.classList.toggle('header__nav-bar--is-open');
 
     }
-
-    onCloseMenu(){
-        this.openClick[0].style.display = "block";
-        this.openClick[0].style.fontSize = '1em';
-
-        this.closeClick[0].style.fontSize = 0;
-        this.closeClick[0].style.transition = 'font-size .5s';
-        this.openMenu[0].style.opacity = 0;
-    }
-
-    moveMenu(){
-
-        this.getStart[0].setAttribute('class','header__icon-start header__icon-start--hidden');
-        this.getStart[0].innerHTML = 'Go';
-
-
-    }
-
-
 
 
 }
